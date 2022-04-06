8da15e83f42be42ff876a07c96ea2d3e3ff7acde
mmschmitz <mmschmitz@users.noreply.github.com>
2022-04-06 13:56:44 -0400

    Update taskscheduler_create.R
    fixed formatting

--- taskscheduler_create.R
+++ taskscheduler_create.R
@@ -3,14 +3,23 @@ list.of.install.packages <- c('taskscheduleR')
 list.of.used.packages <- list.of.install.packages
 new.packages <- list.of.install.packages[!(list.of.install.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0){install.packages(new.packages)}
-lapply(list.of.used.packages, library, character.only = TRUE)###########################
+lapply(list.of.used.packages, library, character.only = TRUE)
+
+###########################
 
 # CRONJOB IMPLEMENTATION? #----
-############################ SETUP PARAMS: # https://rdrr.io/cran/taskscheduleR/man/taskscheduler_create.html - SETUP
+
+###########################
+
+#SETUP PARAMS: # https://rdrr.io/cran/taskscheduleR/man/taskscheduler_create.html - SETUP
 # schedule = c("ONCE", "MONTHLY", "WEEKLY", "DAILY", "HOURLY", "MINUTE", "ONLOGON","ONIDLE"),
 runon_date <- "03/30/2022" #this is the FIRST DATE that you want the cron job to be implemented
 runon_time <- "08:00:00"
-schedule_when <- "DAILY"########################----myscript <- [FULL R SCRIPT PATH HERE]taskscheduler_create(
+schedule_when <- "DAILY"
+
+########################----
+
+myscript <- [FULL R SCRIPT PATH HERE]taskscheduler_create(
 
 taskname = basename(myscript),
 myscript,
